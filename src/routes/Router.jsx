e7c15b9dad69ba81f80dde517a0ffb353d0f1acd
Marito-R-T <47235291+Marito-R-T@users.noreply.github.com>
2022-10-09 20:45:45 -0600

    First commit

---
+++ src/routes/Router.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route, Navigate, useNavigate, useLocation } from "react-router-dom";
+
+export { Router };
+
+function Router() {
+
+    return (
+        <Routes>
+            <Route/>
+            <Route/>
+            <Route/>
+        </Routes>
+    );
+
+}
